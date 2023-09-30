d0535f24ec747960debe72ff478bce54881a2fd9
Gabriel Passos <gabriel80546@gmail.com>
2023-09-30 00:43:58 -0300

    ./push.sh

--- server.js
+++ server.js
@@ -1,14 +1,56 @@
-const http = require('http');
+var http = require('http');
+var fs = require('fs');
+var path = require('path');
 
-const host = '0.0.0.0';
-const port = 3000;
+http.createServer(function (request, response) {
+    console.log('request starting...');
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World!');
-});
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.html';
 
-server.listen(port, host, () => {
-   console.log('Web server running at http://%s:%s',host,port );
-});
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+        case '.json':
+            contentType = 'application/json';
+            break;
+        case '.png':
+            contentType = 'image/png';
+            break;      
+        case '.jpg':
+            contentType = 'image/jpg';
+            break;
+        case '.wav':
+            contentType = 'audio/wav';
+            break;
+    }
+
+    fs.readFile(filePath, function(error, content) {
+        if (error) {
+            if(error.code == 'ENOENT'){
+                fs.readFile('./404.html', function(error, content) {
+                    response.writeHead(200, { 'Content-Type': contentType });
+                    response.end(content, 'utf-8');
+                });
+            }
+            else {
+                response.writeHead(500);
+                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+                response.end(); 
+            }
+        }
+        else {
+            response.writeHead(200, { 'Content-Type': contentType });
+            response.end(content, 'utf-8');
+        }
+    });
+
+}).listen(8125);
+console.log('Server running at http://127.0.0.1:8125/');
