87af8ccf38c572a7cb0e73fbd47684566f5187a2
Gabriel Passos <gabriel80546@gmail.com>
2023-10-13 14:43:44 -0300

    ./push.sh

--- server.js
+++ server.js
@@ -1,57 +1,17 @@
 #!/usr/bin/env node
+
 var http = require('http');
-var fs = require('fs');
-var path = require('path');
+const porta = 8000;
 
-http.createServer(function (request, response) {
-    console.log('request starting...');
+var finalhandler = require('finalhandler');
+var serveStatic = require('serve-static');
 
-    var filePath = '.' + request.url;
-    if (filePath == './')
-        filePath = './index.html';
+var serve = serveStatic("./");
 
-    var extname = path.extname(filePath);
-    var contentType = 'text/html';
-    switch (extname) {
-        case '.js':
-            contentType = 'text/javascript';
-            break;
-        case '.css':
-            contentType = 'text/css';
-            break;
-        case '.json':
-            contentType = 'application/json';
-            break;
-        case '.png':
-            contentType = 'image/png';
-            break;      
-        case '.jpg':
-            contentType = 'image/jpg';
-            break;
-        case '.wav':
-            contentType = 'audio/wav';
-            break;
-    }
+var server = http.createServer(function(req, res) {
+  var done = finalhandler(req, res);
+  serve(req, res, done);
+});
 
-    fs.readFile(filePath, function(error, content) {
-        if (error) {
-            if(error.code == 'ENOENT'){
-                fs.readFile('./404.html', function(error, content) {
-                    response.writeHead(200, { 'Content-Type': contentType });
-                    response.end(content, 'utf-8');
-                });
-            }
-            else {
-                response.writeHead(500);
-                response.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
-                response.end(); 
-            }
-        }
-        else {
-            response.writeHead(200, { 'Content-Type': contentType });
-            response.end(content, 'utf-8');
-        }
-    });
-
-}).listen(8125);
-console.log('Server running at http://127.0.0.1:8125/');
+console.log(`rodando servidor na porta: ${porta}`);
+server.listen(porta);
