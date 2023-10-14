74f6d54273f16167072321a0b48b8a883963683a
Gabriel Passos <gabriel80546@gmail.com>
2023-10-13 23:34:39 -0300

    ./push.sh

--- js/myChess.js
+++
@@ -1,3 +0,0 @@
-console.log(`Chessboard: ${Chessboard} the End`);
-// var board1 = Chessboard(document.querySelector("#board1"), 'start');
-var board1 = Chessboard("#board1", 'start');
---
+++ oldjs/myChess.js
@@ -0,0 +1,2 @@
+var board1 = Chessboard(document.querySelector("#board1"), 'start');
+// var board1 = Chessboard("#board1", 'start');
