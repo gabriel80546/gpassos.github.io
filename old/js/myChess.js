97a4e9f8c12c9c861e46e829613bfb8d7f07c401
Gabriel Passos <gabriel80546@gmail.com>
2023-11-27 16:18:42 -0300

    ./push.sh

--- old/js/myChess.js
+++ old/js/myChess.js
@@ -1,9 +1,11 @@
-var config = {
-    draggable: true,
-    dropOffBoard: 'snapback', // this is the default
-    position: 'start'
-  }
+function onLoad() {
+  var config = {
+      draggable: true,
+      dropOffBoard: 'snapback', // this is the default
+      position: 'start'
+    }
 
-var board1 = Chessboard(document.querySelector("#board1"), config);
-// var board1 = Chessboard(document.querySelector("#board1"), 'start');
-// var board1 = Chessboard("#board1", 'start');
+  var board1 = Chessboard(document.querySelector("#board1"), config);
+  // var board1 = Chessboard(document.querySelector("#board1"), 'start');
+  // var board1 = Chessboard("#board1", 'start');
+}
